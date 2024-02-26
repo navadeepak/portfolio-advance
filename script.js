3a4f2a692bd37f17023afa0025a92ab162653942
Navadeepak.C <128801710+navadeepak@users.noreply.github.com>
2024-02-26 21:23:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openm(){
+    document.getElementById("outer-menu").style="display:none;transition: .5s;";
+    document.getElementById("outer-close").style="display:flex;transition: .5s;";
+    document.getElementById("lis").style="display:flex;transition: .5s;";
+}
+function closem(){
+    document.getElementById("outer-menu").style="display:flex;transition: .5s;";
+    document.getElementById("outer-close").style="display:none;transition: .5s;";
+    document.getElementById("lis").style="display:none;transition: .5s;";
+}
+function web(){
+    location.href="https://drive.google.com/drive/folders/1SacJUcHZ4hQP5ejIy0BeMOF4jHu8_6Wn?usp=drive_link";
+}
+function ai(){
+    location.href="https://drive.google.com/file/d/1qmejj4eyR8lytecnuLk6TFOpIRzb4Ffx/view?usp=sharing";
+}
+function dbms(){
+    location.href="https://drive.google.com/drive/folders/1kfa1VKFDwFZGZ90dWizxd5QIEB9u7jOI?usp=drive_link";
+}
+function java(){
+    location.href="https://drive.google.com/file/d/1dnZa70Wht6IfPxy3_lrtxrozJzj19vCF/view?usp=drive_link";
+}
+function sw(){
+    location.href="https://drive.google.com/file/d/10Ms-JerLHSF7Ci_75gSqefaYgMRg8LaN/view?usp=drive_link";
+}
+function py(){
+    location.href="https://drive.google.com/file/d/11Lx9ih8aR9LbwYVZs6BPpfX97vHWFtkX/view?usp=drive_link";
+}
